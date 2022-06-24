c3b8833a926fb933e70ada8e9ba39a7d1b81d804
soham <sohamdas1038@gmail.com>
2022-06-24 21:20:03 +0530

    updated file

--- Binary-Search/rotations.cpp
+++ Binary-Search/rotations.cpp
@@ -1,32 +1,40 @@
 /****************************************************************
  Suppose a sorted array is rotated x times(circular left shifts),
  i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2].
- Find x (0 <= x <= n-1). Assume no duplicates exist.
+ Find x (0 <= x <= n-1) (effective rotations). Assume no duplicates exist.
 ****************************************************************/
 
 #include<iostream>
 #include<vector>
+#include<climits>
 using namespace std;
 
-int arrRotations(vector<int> arr, int n) {
-    if(arr[0] < arr[n-1]) return 0;
+// function returns the index of the pivot (max element) of the rotated sorted array
+int max_pivotIndex(vector<int> arr, int n) {
+    if(arr[0] < arr[n-1] || n == 1) return (n - 1);
     int s = 0, e = n-1;
-    int mid = s + (e - s) / 2;
+    int mid = (s + e) / 2;
     int ans = -1;
-    
-    while(s <= e) {
-        if(arr[mid] > arr[mid + 1]) {
-            ans = mid;
-            break;
-        }
-        else {
-            
-        }
+    if(arr[0] == arr[mid] && arr[mid] == arr[e]) return (n - 1);
+    else if(mid && arr[mid] < arr[mid - 1]) return (mid - 1);
+    int value = INT_MIN;
 
-        mid = s + (e - s) / 2;
+    while(s <= e) {
+        if((arr[mid] >= arr[mid + 1] && arr[mid] >= arr[mid - 1] && arr[mid] >= value) || (mid == 0 && arr[mid] >= arr[mid + 1] && arr[mid] >= value)) {
+            ans = mid;
+            value = arr[ans];
+            s = mid + 1;
+        } else if(arr[n - 1] >= arr[mid]) e = mid - 1;
+        else s = mid + 1;
+
+        mid = (s + e) / 2;
     }
 
-    return (n - ans - 1);
+    return ans;
+}
+
+int effectiveArrayRotations(vector<int> &arr, int n) {
+    return (n - 1 - max_pivotIndex(arr, n));
 }
 
 int main() {
@@ -42,7 +50,7 @@ int main() {
         for(int i=0; i<n; i++)
             cin >> arr[i];
         
-        cout << "Rotations made on this array are: " << arrRotations(arr, n) << endl;
+        cout << "Rotations made on this array are: " << effectiveArrayRotations(arr, n) << endl;
     }
 
     return 0;
