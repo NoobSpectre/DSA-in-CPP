d53dc3cf279cdca2c2b2054346c0d9469a526e94
soham <sohamdas1038@gmail.com>
2022-08-01 02:02:22 +0530

    added a new solved question on subsequence of a string

--- String/subsequence.cpp
+++ String/subsequence.cpp
@@ -4,12 +4,12 @@ Given an integer array nums of unique elements, return all possible subsets (the
 The solution set must not contain duplicate subsets. Return the solution in any order.
 
 Example 1:
-  Input: nums = [1,2,3]
-  Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+  Input: nums = "abc"
+  Output: [[],[a],[b],[c],[ab],[bc],[ac],[abc]]
 
 Example 2:
-  Input: nums = [0]
-  Output: [[],[0]]
+  Input: nums = [a]
+  Output: [[],[a]]
 
 */
 
